d26c31fd73e32956f24cd277764ff715c64f2232
Damith <bqu.mobile.dev@gmail.com>
2016-06-29 16:17:46 +0530

    appraisal init

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,28 @@ elixir(function(mix) {
         'public/css/app.min.css');
 });
 
+
+//Main Page
+elixir(function(mix) {
+    mix.styles([
+        ///*Global styles START*/
+        'global/plugins/font-awesome/css/font-awesome.min.css',
+        'global/plugins/simple-line-icons/simple-line-icons.min.css',
+        'global/plugins/bootstrap/css/bootstrap.min.css',
+        'global/plugins/uniform/css/uniform.default.css',
+        ///*Global styles END */
+        ///*Theme styles START*/
+        'global/plugins/select2/select2.css',
+        'global/css/components-rounded.css',
+        'global/css/plugins.css',
+        'admin/layout3/css/layout.css',
+        'admin/layout3/css/themes/default.css',
+        'admin/layout3/css/custom.css'
+        /*Theme styles END*/
+    ],
+        'public/css/a1.min.css');
+});
+
 elixir(function(mix) {
     mix.scripts([
 
@@ -50,20 +72,30 @@ elixir(function(mix) {
             'global/scripts/metronic.js',
             'admin/layout3/scripts/layout.js',
             'admin/layout3/scripts/demo.js'
-            //'global/plugins/carousel-owl-carousel/owl-carousel/owl.carousel.min.js',<!-- slider for products -->
-            ////
-            ///* BEGIN RevolutionSlider*/
-            ////
-            //'global/plugins/slider-revolution-slider/rs-plugin/js/jquery.themepunch.plugins.min.js',
-            //'global/plugins/slider-revolution-slider/rs-plugin/js/jquery.themepunch.revolution.js',
-            //'global/plugins/slider-revolution-slider/rs-plugin/js/jquery.themepunch.tools.min.js',
-            //'frontend/pages/scripts/revo-slider-init.js',
-            /////* END RevolutionSlider */
-            ////
-            //'frontend/layout/scripts/layout.js',
-            //'custom/main.js'
-
 
         ],
         'public/scripts/app.js');
+});
+
+elixir(function(mix) {
+    mix.scripts([
+
+            /////* BEGIN CORE PLUGINS  */
+            'global/plugins/jquery.min.js',
+            'global/plugins/jquery-migrate.min.js',
+            'global/plugins/jquery-ui/jquery-ui.min.js',
+            'global/plugins/bootstrap/js/bootstrap.min.js',
+            'global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js',
+            'global/plugins/jquery-slimscroll/jquery.slimscroll.min.js',
+            'global/plugins/jquery.blockui.min.js',
+            'global/plugins/jquery.cokie.min.js',
+            'global/plugins/uniform/jquery.uniform.min.js',
+            ///* END CORE PLUGINS*/
+            'global/plugins/select2/select2.min.js',
+            'global/scripts/metronic.js',
+            'admin/layout3/scripts/layout.js',
+            'admin/layout3/scripts/demo.js'
+
+        ],
+        'public/scripts/a1.js');
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,218 +1,82 @@
 <!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
-<!--[if !IE]><!-->
 <html lang="en">
-<!--<![endif]-->
-<!-- BEGIN HEAD -->
 <head>
-    <meta charset="utf-8"/>
-    <title>The Survey Base  @yield('title')</title>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta content="" name="description"/>
-    <meta content="" name="author"/>
-    {{ Html::style('css/app.min.css') }}
-    <link rel="shortcut icon" href="favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
 </head>
-<!-- END HEAD -->
-<!-- BEGIN BODY -->
-<!-- DOC: Apply "page-header-menu-fixed" class to set the mega menu fixed  -->
-<!-- DOC: Apply "page-header-top-fixed" class to set the top menu fixed  -->
-<body>
-<!-- BEGIN HEADER -->
-<div class="page-header">
-    <!-- BEGIN HEADER TOP -->
-    <div class="page-header-top">
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
-            <!-- BEGIN LOGO -->
-            <div class="page-logo">
-                <a href="{!! url('/') !!}"><h1>The Survey Base</h1></a>
-            </div>
-            <!-- END LOGO -->
-            <!-- BEGIN RESPONSIVE MENU TOGGLER -->
-            <a href="javascript:;" class="menu-toggler"></a>
-            <!-- END RESPONSIVE MENU TOGGLER -->
-            <!-- BEGIN TOP NAVIGATION MENU -->
-            <div class="top-menu">
-                <ul class="nav navbar-nav pull-right">
+            <div class="navbar-header">
 
-                    <!-- BEGIN USER LOGIN DROPDOWN -->
-                    <li class="dropdown dropdown-user dropdown-dark">
-                        <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                            <img alt="" class="img-circle" src="../../assets/admin/layout3/img/avatar9.jpg">
-                            <span class="username username-hide-mobile">Nick</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-default">
-                            <li>
-                                <a href="login.html">
-                                    <i class="icon-key"></i> Log Out </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <!-- END USER LOGIN DROPDOWN -->
-                </ul>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Laravel
+                </a>
             </div>
-            <!-- END TOP NAVIGATION MENU -->
-        </div>
-    </div>
-    <!-- END HEADER TOP -->
-    <!-- BEGIN HEADER MENU -->
-    <div class="page-header-menu">
-        <div class="container">
-            <!-- BEGIN MEGA MENU -->
-            <!-- DOC: Apply "hor-menu-light" class after the "hor-menu" class below to have a horizontal menu with white background -->
-            <!-- DOC: Remove data-hover="dropdown" and data-close-others="true" attributes below to disable the dropdown opening on mouse hover -->
-            <div class="hor-menu ">
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li>
-                        <a href="index.html">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="{!! url('/appraisals') !!}">Appraisals</a>
-                    </li>
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
                 </ul>
             </div>
-            <!-- END MEGA MENU -->
         </div>
-    </div>
-    <!-- END HEADER MENU -->
-</div>
-<!-- END HEADER -->
-<!-- BEGIN PAGE CONTAINER -->
-<div class="page-container">
-    <!-- BEGIN PAGE HEAD -->
-    <div class="page-head">
-        <div class="container">
-            <!-- BEGIN PAGE TITLE -->
-            <div class="page-title">
-                <h1>Blank Page Layout <small>blank page sample</small></h1>
-            </div>
-            <!-- END PAGE TITLE -->
+    </nav>
 
-            <!-- BEGIN PAGE TOOLBAR -->
-            <div class="page-toolbar">
-            </div>
-            <!-- END PAGE TOOLBAR -->
-        </div>
-    </div>
-    <!-- END PAGE HEAD -->
-    <!-- BEGIN PAGE CONTENT -->
-    <div class="page-content">
-        <div class="container">
-            <!-- BEGIN PAGE BREADCRUMB -->
-            <ul class="page-breadcrumb breadcrumb">
-                <li>
-                    <a href="#">Home</a><i class="fa fa-circle"></i>
-                </li>
-                <li>
-                    <a href="layout_blank_page.html">Features</a>
-                    <i class="fa fa-circle"></i>
-                </li>
-                <li class="active">
-                    Blank Page Layout
-                </li>
-            </ul>
-            <!-- END PAGE BREADCRUMB -->
-            <!-- BEGIN PAGE CONTENT INNER -->
-            <div class="row">
-                <div class="col-md-12">
-                    @yield('content')
-                </div>
-            </div>
-            <!-- END PAGE CONTENT INNER -->
-        </div>
-    </div>
-    <!-- END PAGE CONTENT -->
-</div>
-<!-- END PAGE CONTAINER -->
-<!-- BEGIN PRE-FOOTER -->
-<div class="page-prefooter">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
-                <h2>About</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam dolore.
-                </p>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs12 footer-block">
-                <h2>Subscribe Email</h2>
-                <div class="subscribe-form">
-                    <form action="javascript:;">
-                        <div class="input-group">
-                            <input type="text" placeholder="mail@email.com" class="form-control">
-							<span class="input-group-btn">
-							<button class="btn" type="submit">Submit</button>
-							</span>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
-                <h2>Follow Us On</h2>
-                <ul class="social-icons">
-                    <li>
-                        <a href="javascript:;" data-original-title="rss" class="rss"></a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-original-title="facebook" class="facebook"></a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-original-title="twitter" class="twitter"></a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-original-title="googleplus" class="googleplus"></a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-original-title="linkedin" class="linkedin"></a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-original-title="youtube" class="youtube"></a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-original-title="vimeo" class="vimeo"></a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
-                <h2>Contacts</h2>
-                <address class="margin-bottom-40">
-                    Phone: 800 123 3456<br>
-                    Email: <a href="mailto:info@metronic.com">info@metronic.com</a>
-                </address>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- END PRE-FOOTER -->
-<!-- BEGIN FOOTER -->
-<div class="page-footer">
-    <div class="container">
-        {!! date("Y") !!} &copy; Metronic. All Rights Reserved.
-    </div>
-</div>
-<div class="scroll-to-top">
-    <i class="icon-arrow-up"></i>
-</div>
-<!-- END FOOTER -->
-<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-<!-- BEGIN CORE PLUGINS -->
-<!--[if lt IE 9]>
-<script src="../../assets/global/plugins/respond.min.js"></script>
-<script src="../../assets/global/plugins/excanvas.min.js"></script>
-<![endif]-->
+    @yield('content')
 
-{{ Html::script('scripts/app.js') }}
-<script>
-    jQuery(document).ready(function() {
-        Metronic.init(); // init metronic core components
-        Layout.init(); // init current layout
-        Demo.init(); // init demo features
-    });
-</script>
-<!-- END JAVASCRIPTS -->
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
-<!-- END BODY -->
 </html>
