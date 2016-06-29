5b40839bbf7c7c8bddbbbfe9a4bbe8f5c254d101
Damith <bqu.mobile.dev@gmail.com>
2016-06-29 16:50:58 +0530

    patch fix

--- config/app.php
+++ config/app.php
@@ -162,13 +162,6 @@ return [
 
         Intervention\Image\ImageServiceProvider::class
 
-        Laravel\Socialite\SocialiteServiceProvider::class,
-
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-
-        Intervention\Image\ImageServiceProvider::class
-
     ],
 
     /*
@@ -219,10 +212,6 @@ return [
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class
 
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
